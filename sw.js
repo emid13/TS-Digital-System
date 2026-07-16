4d67e4ba1a858ea4fc1d503d7deb0f1d42cd258e
emid13 <elmid13@gmail.com>
2026-07-16 18:01:16 +0700

    Add service worker with caching functionality
    Implement service worker for caching files.

--- sw.js
+++ sw.js
@@ -1,1 +1,22 @@
+const CACHE_NAME = "atsc-pro-v1";
 
+const FILES = [
+  "./",
+  "./index.html",
+  "./css/style.css",
+  "./js/app.js"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(FILES))
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
